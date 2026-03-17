4e5add59f3be576a8ce108503e3452770b42c8a9
Nik6957 <wg23ta@brocku.ca>
2026-03-17 08:16:44 +0000

    Implemented CRUD

--- main.py
+++ main.py
@@ -13,12 +13,26 @@ db.execute('CREATE TABLE IF NOT EXISTS rewards (id INTEGER PRIMARY KEY AUTOINCRE
 # Create
 @app.post("/rewards")
 def create_reward(name: str, points: int):
-    return {"status": "success", "added": name, "points": points}
+    db.execute('INSERT INTO rewards (name, points) VALUES (?, ?)', (name, points))
+    db.commit()
+    return {"message": "Points added"}
 
 # Read
-@app.get("/")
+@app.get("/rewards")
 def read_rewards():
-    return {"message": "Reward Program API"}
+    curent = db.execute('SELECT * FROM rewards')
+    return [{"id": row[0], "name": row[1], "points": row[2]} for row in curent.fetchall()]
 
 # Update
+@app.put("/rewards/{reward_id}")
+def update_reward(reward_id: int, points: int):
+    db.execute('UPDATE rewards SET points = ? WHERE id = ?', (points, reward_id))
+    db.commit()
+    return {"message": "Updated points."}
+
 # Delete
+@app.delete("/rewards/{reward_id}")
+def delete_reward(reward_id: int):
+    db.execute('DELETE FROM rewards WHERE id = ?', (reward_id,))
+    db.commit()
+    return {"message": "Deleted points"}
