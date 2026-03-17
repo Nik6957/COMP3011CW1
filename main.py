93f2896531d119238f4ad4e5967f5567529f141d
Nik6957 <wg23ta@brocku.ca>
2026-03-17 08:07:24 +0000

    Set up CRUD before DB Setup

--- main.py
+++ main.py
@@ -2,11 +2,15 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-
-@app.get("/")
-def read_root():
-    return {"message": "Reward Program API"}
-
+# Create
 @app.post("/rewards")
 def create_reward(name: str, points: int):
     return {"status": "success", "added": name, "points": points}
+
+# Read
+@app.get("/")
+def read_rewards():
+    return {"message": "Reward Program API"}
+
+# Update
+# Delete
